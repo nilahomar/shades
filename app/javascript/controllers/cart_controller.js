f09118ebf331068ca51e3a87cda0109ab7779477
Nila Homar <nilahomar@gmail.com>
2023-03-09 15:11:26 +0100

    cart item

---
+++ app/javascript/controllers/cart_controller.js
@@ -0,0 +1,33 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="cart"
+export default class extends Controller {
+  static values = {
+    product: String
+  }
+
+
+  static targets = ['shoppingCartCircle']
+
+
+
+  connect() {
+    console.log("hello from js")
+    this.shoppingCartState = []
+    console.log(this.shoppingCartCircleTarget)
+    console.log(this.productValue)
+
+  }
+
+
+  updateShopping(){
+    console.log("Hello I have been clicked")
+    let item = {}
+    item.id = this.productValue
+    this.shoppingCartState.push(item)
+    this.shoppingCartCircleTarget.style.display = "block"
+    this.shoppingCartCircleTarget.innerHTML = `<h6>${this.shoppingCartState.length}</h6>`
+    console.log(this.shoppingCartState)
+
+  }
+}
