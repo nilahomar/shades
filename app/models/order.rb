b3dced2c6bd8244dd1102ac484b6c19a388b05e2
James Hibbeard <jhibbeard@gmail.com>
2023-03-09 17:02:42 +0100

    cleaned up migration and relationships

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :user
+  has_many :orderProducts
+  has_many :sub_products, through: :orderProducts
 end
--- app/models/sub_product.rb
+++ app/models/sub_product.rb
@@ -1,4 +1,6 @@
 class SubProduct < ApplicationRecord
   belongs_to :product
   has_many :color_shade
+  has_many :orderProducts
+  has_many :orders, through: :orderProducts
 end
