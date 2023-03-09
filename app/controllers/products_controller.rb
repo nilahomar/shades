c6ebb42a65314aa3a62135b91e33b9f466dc3708
Alexandra Campos <alexandra.campos@gmx.de>
2023-03-09 12:34:23 +0100

    navbar

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,10 @@
 class ProductsController < ApplicationController
   def index
+    if params[:query].present?
+      sql_query = "brand_name ILIKE :query"
+      @products = Product.where(sql_query, query: "%#{params[:query]}%")
+    else
     @products = Product.all
+    end
   end
 end
