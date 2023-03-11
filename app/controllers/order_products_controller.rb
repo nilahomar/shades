2f72983de4ce00376482e205e2d5ba8c77049f6e
thooba siddiqa thooba.siddiqa@gmail.com <thooba.siddiqa@gmail.com>
2023-03-11 23:28:28 +0100

    added cart flow

--- app/controllers/order_products_controller.rb
+++ app/controllers/order_products_controller.rb
@@ -1,37 +1,36 @@
 class OrderProductsController < ApplicationController
   before_action :authenticate_user!
+
+  def index
+    @cart = current_user.order_products.where(status: true)
+  end
+
   def show
     @order_product = OrderProduct.find(params[:id])
   end
 
-
-
   def custom_create
+    @order_product = OrderProduct.new
+    @sub_product = SubProduct.find(params[:subproduct])
 
-      @order_product = OrderProduct.new
-
-      @sub_product = SubProduct.find(params[:subproduct])
-
-      @order_product.user_id = current_user.id
-
-      @order_product.sub_product_id = @sub_product.id
-      @order_product.user = current_user
-
-      @order_product.save!
-      redirect_to sub_product_path(@sub_product)
+    @order_product.user_id = current_user.id
+    @order_product.sub_product_id = @sub_product.id
+    @order_product.user = current_user
+    @order_product.status = true
+    @order_product.quantity = 1
+    @order_product.save!
+    redirect_to sub_product_path(@sub_product)
   end
 
   def destroy
     @order_product = OrderProduct.find(params[:id])
     @order_product.destroy
-    redirect_to my_cart_path, status: :see_other
+    redirect_to order_products_path, status: :see_other
   end
-
 
   private
 
   def orderproducts_params
     params.require(:restaurant).permit(:name, :address, :rating)
   end
-
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,10 +4,16 @@ class OrdersController < ApplicationController
   end
 
   def show
-    @cart = current_user.order_products
   end
 
-  def destroy
-
+  def create
+    # current_user.order_products.where(status: true).each do |item|
+    #   Order.create({
+    #     user_id: item.user_id,
+    #     sub_product_id: item.sub_product_id,
+    #     quantity: item.quantity,
+    #     price: item.sub_product.product.price
+    #   })
+    # end
   end
 end
