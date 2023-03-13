05a8b1f883e9fd34865e9059d294a16cdb5c77f6
thooba siddiqa thooba.siddiqa@gmail.com <thooba.siddiqa@gmail.com>
2023-03-13 23:59:33 +0100

    created path for img upload

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :products, only: [:index]
-  resources :color_shades, only: [:index] do
+  resources :color_shades, only: [:index, :create] do
     resources :sub_products, only: [:index]
   end
   resources :sub_products, only: [:show]
   resources "order_products", only: [:index, :destroy]
   resources "orders", only: [:index, :create, :update]
+  get "my_shades", to: "color_shades#image_upload", as: :my_shades
 
   # get "my_orders", to: "orders#my_orders", as: :my_orders
   post "order_products", to: "order_products#custom_create", as: :order_product_custom_create
