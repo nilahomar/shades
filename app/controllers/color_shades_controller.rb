58927e1a1ad8c989826295c1d3f6e2c257a72c09
thooba siddiqa thooba.siddiqa@gmail.com <thooba.siddiqa@gmail.com>
2023-03-14 00:02:23 +0100

    created imageupload and create method

--- app/controllers/color_shades_controller.rb
+++ app/controllers/color_shades_controller.rb
@@ -2,4 +2,12 @@ class ColorShadesController < ApplicationController
   def index
     @color_shades = ColorShade.all
   end
+
+  def image_upload
+    @color_shades = ColorShade.all
+  end
+
+  def create
+    current_user.pic.attach(params[:this_user][:pic])
+  end
 end
