51459a369127b71ce99b0e61f90f489d27e8a99a
Nila Homar <nilahomar@gmail.com>
2023-03-14 10:37:25 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_03_13_231334) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -134,10 +133,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_13_231334) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "favourites", "products"
   add_foreign_key "favourites", "sub_products"
   add_foreign_key "favourites", "users"
   add_foreign_key "order_products", "sub_products"
